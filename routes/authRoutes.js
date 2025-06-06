0e121dcc74cec129c31d000094d44044e9a289f4
RichardKyalo <richardkyalo57@gmail.com>
2025-06-06 16:58:39 +0300

    decoding loged in user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,11 +3,15 @@ import { registerUser } from '../controllers/authController.js';
 import { validateRegister } from '../middlewares/validateRegister.js';
 import { loginUser } from '../controllers/authLoginController.js';
 import { validateLogin } from '../middlewares/validateLogin.js';
+import { protect } from '../middlewares/authMiddleware.js';
 
 
 const router = express.Router();
 
 router.post('/register', validateRegister, registerUser);
 router.post('/login', validateLogin, loginUser);
+router.get('/currentUser', protect, (req, res) => {
+    res.json({user: req.user})
+})
 
 export default router;
